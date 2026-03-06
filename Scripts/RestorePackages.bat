942846e7a0253b08ca7e03815d554944ef97ff34
Number:503 <155703779+unnamed-user503@users.noreply.github.com>
2026-03-06 21:48:16 +0900

    パッケージ復元時のMSBuildを自動で探すように修正

--- Scripts/RestorePackages.bat
+++ Scripts/RestorePackages.bat
@@ -5,10 +5,29 @@ chcp 65001 > nul
     setlocal
 
     :: 固定パスとファイル名
-    set "SOLUTION=Workspace.sln"
+    set "SOLUTION=Workspace.slnx"
     set "RESTORE_PATH=.\Packages"
-    set "MSBUILD=C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\msbuild.exe"
+    :: set "MSBUILD=C:\Program Files\Microsoft Visual Studio\2026\Community\MSBuild\Current\Bin\msbuild.exe"
     set "ROOT=%~dp0\.."
+
+    :: vswhereを使って最新のMSBuildのパスを動的に取得
+    set "VSWHERE=%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe"
+    
+    for /f "usebackq tokens=*" %%i in (`"%VSWHERE%" -latest -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe`) do (
+        set "MSBUILD=%%i"
+    )
+
+    :: パスが見つからなかった場合のバックアップ用エラー
+    if not exist "%MSBUILD%" (
+        echo [ERROR] MSBuild が見つかりませんでした。
+        pause
+        exit /b 1
+    )
+
+    if not exist "%SOLUTION%" (
+        echo [ERROR] ソリューションファイルが見つかりません: %SOLUTION%
+        exit /b 1
+    )
 
     pushd "%ROOT%"
     call :main
@@ -18,24 +37,6 @@ chcp 65001 > nul
     exit /b
 
 :main
-    call :check_environment || goto :end
-    call :restore_packages
-    goto :end
-
-:check_environment
-    if not exist "%SOLUTION%" (
-        echo [ERROR] ソリューションファイルが見つかりません: %SOLUTION%
-        exit /b 1
-    )
-
-    if not exist "%MSBUILD%" (
-        echo [ERROR] MSBuild が見つかりません: %MSBUILD%
-        exit /b 1
-    )
-
-    exit /b 0
-
-:restore_packages
     echo [INFO] パッケージの復元を開始します...
     echo        ソリューション: %SOLUTION%
     echo        パス: %RESTORE_PATH%
